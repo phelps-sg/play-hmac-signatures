83af72257feafc41ecfa057e6c04a7c38d0842cf
Steve Phelps <sphelps@sphelps.net>
2022-11-10 21:01:46 +0000

    implemented unit test for case where signatures are supplied but are not cryptographically valid

--- src/test/scala/ControllerTests.scala
+++ src/test/scala/ControllerTests.scala
@@ -15,6 +15,7 @@ import play.api.mvc.BodyParsers
 import play.api.test.Helpers.{POST, defaultAwaitTimeout, status}
 import play.api.test.{FakeRequest, Helpers}
 
+import scala.collection.immutable.ArraySeq
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class ControllerTests extends AnyWordSpecLike with should.Matchers {
@@ -35,12 +36,29 @@ class ControllerTests extends AnyWordSpecLike with should.Matchers {
       Helpers.stubControllerComponents(),
       slackSignatureVerifyAction
     )
-
-    "return a 401 error when not supplying signatures" in {
-      val body = ByteString(
+    val body = ByteString(
         Json.parse(""" { "message" : "Hello world!" } """).toString()
       )
-      val fakeRequest = FakeRequest(POST, "/").withBody(body)
+
+    "return a 401 error when not supplying signatures" in {
+     val fakeRequest = FakeRequest(POST, "/").withBody(body)
+      val result = testController.test().apply(fakeRequest)
+      status(result) mustEqual UNAUTHORIZED
+    }
+
+    "return a 401 error when supplying invalid signatures" in {
+      val invalidSignatureHeaders = Array(
+        ("X-Slack-Request-Timestamp", "1663156082"),
+        (
+          "X-Slack-Signature",
+          "v0=d1c387a20da72e5e07de4e2fb7e93cd9b44c2caa118868aad99c3b20c93de73a"
+        )
+      )
+      val fakeRequest = FakeRequest(POST, "/")
+        .withBody(body)
+        .withHeaders(
+          ArraySeq.unsafeWrapArray(invalidSignatureHeaders): _*
+        )
       val result = testController.test().apply(fakeRequest)
       status(result) mustEqual UNAUTHORIZED
     }
