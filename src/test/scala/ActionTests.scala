cd21bebb2024ee37f4e0c0c8735be26452cdf440
Steve Phelps <sphelps@sphelps.net>
2022-12-07 10:22:33 +0000

    additional test to verify expectedSignature method in SlackSignatureVerifyAction

--- src/test/scala/ActionTests.scala
+++ src/test/scala/ActionTests.scala
@@ -3,23 +3,33 @@
  */
 
 import akka.util.ByteString
-import com.mesonomics.playhmacsignatures.EpochSeconds
+import com.mesonomics.playhmacsignatures.{EpochSeconds, HmacSignature}
 import org.scalatest.matchers.should
 import org.scalatest.wordspec.AnyWordSpecLike
 
 class ActionTests extends AnyWordSpecLike with should.Matchers {
 
   trait TestFixtures extends CommonFixtures {
+    val timeSeconds: Long = 1000
     val timestamp: EpochSeconds = EpochSeconds(1000)
-    val body: ByteString = ByteString("test-body")
+    val bodyStr: String = "test-body"
+    val body: ByteString = ByteString(bodyStr)
+    val signatureBytes: Array[Byte] = Array(0xff, 0x00, 0xf0).map(_.toByte)
   }
 
   "SlackSignatureVerifyAction" should {
+
     "compute the correct payload" in new TestFixtures {
       slackSignatureVerifyAction.payload(
         timestamp,
         body
-      ) shouldBe "v0:1000:test-body"
+      ) shouldBe s"v0:$timeSeconds:$bodyStr"
+    }
+
+    "compute the correct expectedSignature" in new TestFixtures {
+      slackSignatureVerifyAction.expectedSignature(
+        signatureBytes
+      ) shouldEqual HmacSignature("v0=ff00f0")
     }
   }
 }
