cc456e0b2bc4fd9d635fdbc71356ea77bd8727ed
Scala Steward <scala_steward@virtuslab.com>
2023-07-18 18:24:16 +0000

    Update play to 2.8.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies extends AutoPlugin {
     val scala212 = "2.12.18"
     val scala213 = "2.13.11"
 
-    val play = "2.8.19"
+    val play = "2.8.20"
     val guice = "7.0.0"
     val jackson = "2.13.4"
 
